c4350fb3283591bae3ba0569f6da22a79252e067
fabio-cumbo <fabio.cumbo@gmail.com>
2022-04-25 18:32:59 -0400

    Building setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import sys, setuptools
+
+if sys.version_info[0] < 3:
+    sys.stdout.write("hdlib requires Python 3 or higher. Your Python your current Python version is {}.{}.{}"
+                     .format(sys.version_info[0], sys.version_info[1], sys.version_info[2]))
+
+setuptools.setup(name='hdlib',
+                 version='0.1.0',
+                 author='Fabio Cumbo',
+                 author_email='fabio.cumbo@gmail.com',
+                 url='http://github.com/fabio-cumbo/hdlib',
+                 license='LICENSE',
+                 packages=setuptools.find_packages(),
+                 description='Hyperdimensional Computing Library for building Vector Symbolic Architectures in Python',
+                 long_description=open('README.md').read(),
+                 long_description_content_type='text/markdown',
+                 install_requires=[
+                     "numpy"
+                 ],
+                 zip_safe=False)
