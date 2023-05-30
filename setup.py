e3eaaa1d4020d3eddcd1c4338b169a73baac2af8
fabio-cumbo <fabio.cumbo@gmail.com>
2023-05-30 18:28:20 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,33 @@
-import sys, setuptools
+import setuptools
+import sys
+
+from hdlib import __version__
 
 if sys.version_info[0] < 3:
     sys.stdout.write("hdlib requires Python 3 or higher. Your Python your current Python version is {}.{}.{}"
                      .format(sys.version_info[0], sys.version_info[1], sys.version_info[2]))
 
-setuptools.setup(name='hdlib',
-                 version='0.1.0',
-                 author='Fabio Cumbo',
-                 author_email='fabio.cumbo@gmail.com',
-                 url='http://github.com/cumbof/hdlib',
-                 license='LICENSE',
-                 packages=setuptools.find_packages(),
-                 description='Hyperdimensional Computing Library for building Vector Symbolic Architectures in Python',
-                 long_description=open('README.md').read(),
-                 long_description_content_type='text/markdown',
-                 install_requires=[
-                     "numpy"
-                 ],
-                 zip_safe=False)
+setuptools.setup(
+    author="Fabio Cumbo",
+    author_email="fabio.cumbo@gmail.com",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering",
+    ],
+    description="Hyperdimensional Computing Library for building Vector Symbolic Architectures in Python",
+    install_requires=["numpy>=1.22.3"],
+    license="MIT",
+    license_files=["LICENSE"],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    name="hdlib",
+    packages=setuptools.find_packages(),
+    python_requires=">=3",
+    url="http://github.com/cumbof/hdlib",
+    version=__version__,
+    zip_safe=False
+)
