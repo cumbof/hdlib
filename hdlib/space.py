613c8836498c0e45565b665964d4717be63b17d4
fabio-cumbo <fabio.cumbo@gmail.com>
2023-05-31 15:29:38 -0400

    Updating space.py
    improving performances; Add add_tag and remove_tag methods; Dump and load vectors to and from pickle file

--- hdlib/space.py
+++ hdlib/space.py
@@ -7,7 +7,7 @@ import pickle
 import uuid
 
 import numpy as np
-from typing import List, Optional, Tuple, Union
+from typing import List, Optional, Set, Tuple, Union
 
 
 class Vector(object):
@@ -18,10 +18,13 @@ class Vector(object):
     def __init__(
         self,
         name: Optional[str]=None,
-        size: int=1000,
+        size: int=10000,
         vector: Optional[np.ndarray]=None,
-        tags: Optional[List[Union[str, int, float]]]=None,
-        seed: Optional[int]=None
+        vtype: Optional[str]="bipolar",
+        tags: Optional[Set[Union[str, int, float]]]=None,
+        seed: Optional[int]=None,
+        warning: bool=False,
+        from_file: Optional[os.path.abspath]=None,
     ) -> "Vector":
         """
         Initialize a Vector object
@@ -29,8 +32,11 @@ class Vector(object):
         :param name:        Vector name or id
         :param size:        Vector size
         :param vector:      Vector as numpy.ndarray
-        :param tags:        Tags for grouping vectors
+        :param vtype:       Vector type: bipolar or binary
+        :param tags:        Set of tags for grouping vectors
         :param seed:        Random seed for reproducibility
+        :param warning:     Print warning messages if True
+        :param from_file:   Load a vector from pickle file
         :return:            A Vector object
         """
 
@@ -47,34 +53,64 @@ class Vector(object):
             self.name = name
 
         except:
-            raise TypeError("Vector name must be an instance of a primitive")
-
-        # Conditions on vector size
-        # It must be an integer number greater than 1000
-        if not isinstance(size, int):
-            raise TypeError("Vector size must be an integer number")
-
-        if size < 1000:
-            raise ValueError("Vector size must be greater than 1000")
-
-        self.size = size        
+            raise TypeError("Vector name must be instance of a primitive")
 
         # Register random seed for reproducibility 
         self.seed = seed
 
         # Add tags
-        self.tags = tags if tags else list()
+        self.tags = tags if tags else set()
 
         # Conditions on vector
         # It must be a Numpy ndarray
-        # A random bipolar vector is generated if not specified
+        # A random vector is generated if not specified
         if vector is not None:
             if not isinstance(vector, np.ndarray):
-                raise TypeError("Vector must be an instance of numpy.ndarray")
+                raise TypeError("Vector must be instance of numpy.ndarray")
 
             self.vector = vector
 
+            self.size = len(self.vector)
+
+            if self.size < 10000:
+                raise ValueError("Vector size must be greater than or equal to 10000")
+
+            self.vtype = vtype
+
+            # Try to infer the vector type from the content of the vector itself
+            if ((self.vector == 0) | (self.vector == 1)).all():
+                self.vtype = "binary"
+
+            elif ((self.vector == -1) | (self.vector == 1)).all():
+                self.vtype = "bipolar"
+
+            else:
+                if warning:
+                    print("Vector type can be binary or bipolar only")
+
+        elif from_file and os.path.isfile(from_file):
+            # Load vector from pickle file
+            with open(from_file, "rb") as pkl:
+                self.name, self.size, self.vector, self.vtype, self.tags, self.seed = pickle.load(pkl)
+
         else:
+            # Conditions on vector size
+            # It must be an integer number greater than or equal to 10000
+            # This size makes sure that vectors are quasi-orthogonal in space
+            if not isinstance(size, int):
+                raise TypeError("Vector size must be an integer number")
+
+            if size < 10000:
+                raise ValueError("Vector size must be greater than or equal to 10000")
+
+            self.size = size
+
+            # Add vector type
+            self.vtype = vtype.lower()
+
+            if vtype not in ("bipolar", "binary"):
+                raise ValueError("Vector type can be binary or bipolar only")
+
             if seed is None:
                 rand = np.random
 
@@ -86,8 +122,12 @@ class Vector(object):
 
                 rand = np.random.default_rng(seed=self.seed)
 
-            # Build a random bipolar vector
-            self.vector = 2*rand.randint(2, size=size)-1
+            # Build a random binary vector
+            self.vector = rand.randint(2, size=size)
+
+            if vtype == "bipolar":
+                # Build a random bipolar vector
+                self.vector = 2 * self.vector - 1
 
     def __len__(self) -> int:
         """
@@ -106,13 +146,28 @@ class Vector(object):
         :return:            Cosine similarity
         """
 
-        if not isinstance(vector, Vector):
-            raise TypeError("Input must be a Vector object")
-
         if self.size != vector.size:
             raise Exception("Vectors must have the same size")
 
         return np.dot(self.vector, vector.vector) / (np.linalg.norm(self.vector) * np.linalg.norm(vector.vector))
+
+    def dump(self, to_file: Optional[os.path.abspath]=None) -> None:
+        """
+        Dump the hyperdimensional vector to a pickle file
+
+        :param to_file:     Path to the output pickle file
+        """
+
+        if not to_file:
+            # Dump the vector to a pickle file in the current working directory
+            # if not file path is provided
+            to_file = os.path.join(os.getcwd, "{}.pkl".format(self.name))
+
+        if os.path.isfile(to_file):
+            raise Exception("The output file already exists!\n{}".format(to_file))
+
+        with open(to_file, "wb") as pkl:
+            pickle.dump((self.name, self.size, self.vector, self.vtype, self.tags, self.seed), pkl)
 
 
 class Space(object):
@@ -120,12 +175,13 @@ class Space(object):
     Vectors space
     """
 
-    def __init__(self, size: int=1000, from_file: Optional[os.path.abspath]=None) -> "Space":
+    def __init__(self, size: int=10000, vtype: Optional[str]="bipolar", from_file: Optional[os.path.abspath]=None) -> "Space":
         """
         Initialize the vectors space as a dictionary of Vector objects
 
         :param size:        Size of vectors in the space
-        :param from_file:   Load a space from file
+        :param vtype:       Vector type: bipolar or binary
+        :param from_file:   Load a space from pickle file
         :return:            A Space object
         """
 
@@ -133,8 +189,27 @@ class Space(object):
 
         self.size = size
 
+        if self.size < 10000:
+            raise ValueError("Size of vectors in space must be greater than or equal to 10000")
+
+        self.vtype = vtype.lower()
+
+        if self.vtype not in ["binary", "bipolar"]:
+            raise ValueError("Vector type not supported")
+
+        self.tags = dict()
+
         if from_file and os.path.isfile(from_file):
-            self.size, self.space = pickle.load(open(from_file, "rb"))
+            with open(from_file, "rb") as pkl:
+                self.size, self.vtype, self.space = pickle.load(pkl)
+
+            for name in self.space:
+                if self.space[name].tags:
+                    for tag in self.space[name].tags:
+                        if tag not in self.tags:
+                            self.tags[tag] = set()
+
+                        self.tags[tag].add(name)
 
     def __len__(self) -> int:
         """
@@ -170,29 +245,29 @@ class Space(object):
         if not names and not tags:
             raise Exception("No names or tags provided!")            
 
-        try:
-            vectors = list()
+        vectors = set()
 
-            if names:
+        if names:
+            try:
                 names = [str(name) for name in names]
+            
+            except:
+                raise TypeError("Vector name must be instance of a primitive")
 
-                for vector_name in names:
-                    if vector_name in self.space:
-                        vectors.append(self.space[vector_name])
+            for vector_name in names:
+                if vector_name in self.space:
+                    vectors.add(self.space[vector_name])
 
-            elif tags:
-                for tag in tags:
-                    if not isinstance(tag, str) and not isinstance(tag, int) and not isinstance(tag, float):
-                        raise TypeError("Tags must be string, integer, or float")
+        elif tags:
+            for tag in tags:
+                if not isinstance(tag, str) and not isinstance(tag, int) and not isinstance(tag, float):
+                    raise TypeError("Tags must be string, integer, or float")
 
-                for vector_name in self.space:
-                    if set(tags).intersection(self.space[vector_name].tags):
-                        vectors.append(self.space[vector_name])
+                if tag in self.tags:
+                    for vector_name in self.tags[tag]:
+                        vectors.add(self.space[vector_name])
 
-            return vectors
-
-        except:
-            raise TypeError("Input must be an instance of a primitive")
+        return list(vectors)
 
     def insert(self, vector: Vector) -> None:
         """
@@ -201,16 +276,22 @@ class Space(object):
         :param vector:      Vector object
         """
 
-        if not isinstance(vector, Vector):
-            raise TypeError("Input must be a Vector object")
-
         if self.size != vector.size:
             raise Exception("Space and vectors with different size are not compatible")
+
+        if self.vtype != vector.vtype:
+            raise Exception("Attempting to insert a {} vector into a {} space: failed".format(vector.vtype, self.vtype))
 
         if vector.name in self.space:
             raise Exception("Vector \"{}\" already in space".format(vector.name))
 
         self.space[vector.name] = vector
+
+        for tag in vector.tags:
+            if tag not in self.tags:
+                self.tags[tag] = set()
+
+            self.tags[tag].add(vector.name)
 
     def bulkInsert(
         self,
@@ -241,21 +322,27 @@ class Space(object):
             try:
                 name = str(name)
 
-                if name in self.space:
-                    if not ignoreExisting:
-                        raise Exception("Vector \"{}\" already in space".format(name))
-
-                    else:
-                        continue
-
-                vector_tags = tags[pos] if tags else list()
-
-                vector = Vector(name=name, size=self.size, tags=vector_tags)
-
-                self.space[vector.name] = vector
-
             except:
                 raise TypeError("Entries in input list must be instances of primitives")
+
+            if name in self.space:
+                if not ignoreExisting:
+                    raise Exception("Vector \"{}\" already in space".format(name))
+
+                else:
+                    continue
+
+            vector_tags = set(tags[pos]) if tags else set()
+
+            vector = Vector(name=name, size=self.size, tags=vector_tags, vtype=self.vtype)
+
+            self.space[vector.name] = vector
+
+            for tag in vector_tags:
+                if tag not in self.tags:
+                    self.tags[tag] = set()
+
+                self.tags[tag].add(name)
 
     def remove(self, name: str) -> Vector:
         """
@@ -268,17 +355,78 @@ class Space(object):
         try:
             name = str(name)
 
-            if name in self.space:
-                vector = self.space[name]
+        except:
+            raise TypeError("Vector name must be instance of a primitive")
 
-                del self.space[name]
+        if name in self.space:
+            vector = self.space[name]
 
-                return vector
+            del self.space[name]
 
-            raise Exception("Vector not in space")
+            for tag in vector.tags:
+                self.tags[tag].remove(vector.name)
+
+                if not self.tags[tag]:
+                    del self.tags[tag]
+
+            return vector
+
+        raise Exception("Vector not in space")
+
+    def add_tag(self, name: str, tag: Union[str, int, float]) -> None:
+        """
+        Tag a vector
+
+        :param name:    Vector name or ID
+        :param tag:     Vector tag
+        """
+
+        try:
+            name = str(name)
 
         except:
-            raise TypeError("Input must be an instance of a primitive")
+            raise TypeError("Vector name must be instance of a primitive")
+
+        if name not in self.space:
+            raise Exception("Vector not in space")
+
+        if not isinstance(tag, str) and not isinstance(tag, int) and not isinstance(tag, float):
+            raise TypeError("Tags must be string, integer, or float")
+
+        self.space[name].tags.add(tag)
+
+        if tag not in self.tags:
+            self.tags[tag] = set()
+
+        self.tags[tag].add(name)
+
+    def remove_tag(self, name: str, tag: Union[str, int, float]) -> None:
+        """
+        Untag a vector
+
+        :param name:    Vector name or ID
+        :param tag:     Vector tag
+        """
+
+        try:
+            name = str(name)
+
+        except:
+            raise TypeError("Vector name must be instance of a primitive")
+
+        if not isinstance(tag, str) and not isinstance(tag, int) and not isinstance(tag, float):
+            raise TypeError("Tags must be string, integer, or float")
+        
+        if name not in self.space:
+            raise Exception("Vector not in space")
+
+        if tag in self.tags:
+            self.space[name].tags.remove(tag)
+
+            self.tags[tag].remove(name)
+
+            if not self.tags[tag]:
+                del self.tags[tag]
 
     def find(self, vector: Vector, threshold: float=-1.0) -> Tuple[str, float]:
         """
@@ -303,9 +451,6 @@ class Space(object):
         :param threshold:   Do not consider entries in memory with a distance lower than this threshold
         :return:            Dictionary with distances of the input vector against all the other vectors in space, and the name of the closest vector in space
         """
-
-        if not isinstance(vector, Vector):
-            raise TypeError("Input must be a Vector object")
 
         if self.size != vector.size:
             raise Exception("Space and vectors with different size are not compatible")
@@ -349,4 +494,4 @@ class Space(object):
             raise Exception("The output file already exists!\n{}".format(to_file))
 
         with open(to_file, "wb") as pkl:
-            pickle.dump((self.size, self.space), pkl)
+            pickle.dump((self.size, self.vtype, self.space), pkl)
